a60b6cf3ec83e66ec7168c2a8f14352b7bd164a8
Rayhatron <rayhatron@gmail.com>
2016-10-26 12:56:37 +0200

    Added sequence creation function

--- scripts/main.js
+++ scripts/main.js
@@ -5,18 +5,45 @@ var app = new Vue({
     message: 'Hello Vue!',
     power: false,
     strictMode: false,
-    count: "00",
+    count: 0,
     blink1: false,
     blink2: false,
     blink3: false,
-    blink4: false
+    blink4: false,
+    sequence: [],
+    userSequence: [],
+    randomSquare: 1
+
   },
   methods: {
   	toggleStrictMode: function (){
   		this.strictMode = !this.strictMode;
   	},
+  
+  	chooseSquare: function(){
+		this.randomSquare = Math.floor((Math.random() * 4) + 1);
+  		console.log(this.randomSquare);
+  	},
+  	createSequence: function(){
+  		this.chooseSquare();
+  		if(this.count === 0){
+  			this.sequence.push(this.randomSquare);
+  			this.count++;
+  		}else{
+  			if(this.randomSquare === this.sequence[this.sequence.length-1]){
+  				this.chooseSquare();
+  				console.log("Looking for a different square.");
+  			}else{
+  				this.sequence.push(this.randomSquare);
+  				this.count++;
+  			}
+  		}
+  		console.log("sequence is: " + this.sequence);
+  		
+  	},
   	startGame: function (){
   		console.log("Starting");
+  		this.createSequence();
   		this.blink1 = true;
   		this.blink2 = true;
   		this.blink3 = true;
