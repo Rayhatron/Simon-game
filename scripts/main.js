c4be6db1c0ffd9551c91cf88af2710c893d9b6af
Rayhatron <rayhatron@gmail.com>
2016-10-26 17:45:28 +0200

    Finished project

--- scripts/main.js
+++ scripts/main.js
@@ -33,8 +33,8 @@ var app = new Vue({
   			this.blinkSequence();
   		}else{
   			if(this.randomSquare === this.sequence[this.sequence.length-1]){
-  				this.chooseSquare();
   				console.log("Looking for a different square.");
+  				this.createSequence();
   			}else{
   				this.sequence.push(this.randomSquare);
   				this.count++;
@@ -48,21 +48,81 @@ var app = new Vue({
   		var self = this;
   			setTimeout(function(){
   				self["blink" + self.sequence[self.i]] = true;
+  				var audio = document.getElementById(self.sequence[self.i]);
   				self.i++;
-  				if(self.i<self.sequence.length){
+  				if(self.i<=self.sequence.length){
+       				audio.play();
   					self.blinkSequence();
   				}else{
-  					self.i = 0;
-  					setTimeout(function(){
-		                self.blink1 = false;
-		                self.blink2 = false;
-		                self.blink3 = false;
-		                self.blink4 = false;
-        			}, 1000);
+  					self.i = 0;	
   				}
   			}, 1000);
+  			setTimeout(function (){
+				self.blink1 = false;
+	            self.blink2 = false;
+	            self.blink3 = false;
+	            self.blink4 = false;
+  			}, 2000);
+  			
+  			
+        
+  	},
+  	setUserSequence: function(squareNum){
+  		var self =  this;
+  		if(this.userSequence.length<this.sequence.length){
+  			this.userSequence.push(squareNum);
+  			var audio = document.getElementById(squareNum);	
+  			audio.play();
+  		}
+  		if(this.strictMode === true && this.compareSequences() === false){
+  				var audio = document.getElementById(5);	
+  				audio.play();
+  				this.userSequence = [];
+				this.sequence = [];
+				this.count = 0;
+				this.randomSquare = 0;
+				this.i = 0;
+
+  				setTimeout(function(){
+  					self.startGame();
+  				}, 2000);
   				
-            
+  				console.log("FAiled on strict" + this.count  + "seq " + this.sequence + "ussq " + this.userSequence);
+  				return;
+  			}
+
+  		if(this.compareSequences() === false){
+  			var audio = document.getElementById(5);	
+  			audio.play();
+  			this.userSequence = [];
+  			setTimeout(function(){
+  				self.blinkSequence();
+
+  			},1000);
+  			return false;
+  		}else{
+  			if(this.userSequence.length === this.sequence.length && this.compareSequences()){
+  				this.userSequence = [];
+  				setTimeout(function(){
+  					self.createSequence();
+
+  				}, 2000);
+  				console.log("user sequ: " + this.userSequence);
+  			}
+  			
+  		}
+  		
+  		
+  		console.log(this.userSequence);
+		
+  	},
+  	compareSequences: function(){
+		var i, l = Math.min(this.sequence.length, this.userSequence.length); 
+
+		  for (i=0; i<l; i++) {
+		    if (this.sequence[i] !== this.userSequence[i]) return false;
+		  }
+		  return true;
   	},
   	startGame: function (){
   		console.log("Starting");
