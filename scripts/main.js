da21611669f35255a070e2743adaeddb3a6ef860
Rayhatron <rayhatron@gmail.com>
2016-10-26 12:30:18 +0200

    Added blink animation for each button

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,33 @@ var app = new Vue({
   el: '#container',
   data: {
     message: 'Hello Vue!',
-    checked: false
+    power: false,
+    strictMode: false,
+    count: "00",
+    blink1: false,
+    blink2: false,
+    blink3: false,
+    blink4: false
+  },
+  methods: {
+  	toggleStrictMode: function (){
+  		this.strictMode = !this.strictMode;
+  	},
+  	startGame: function (){
+  		console.log("Starting");
+  		this.blink1 = true;
+  		this.blink2 = true;
+  		this.blink3 = true;
+  		this.blink4 = true;
+  		var self = this;
+            setTimeout(function(){
+                self.blink1 = false;
+                self.blink2 = false;
+                self.blink3 = false;
+                self.blink4 = false;
+            }, 1000);
+  	}
   }
+
 })
 }
